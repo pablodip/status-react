0873765adca6878cda88ab5922f8cab0e2ed590c
pablodip <pablodip@gmail.com>
2017-11-23 09:20:04 +0100

    supported languages and trans ids

--- src/status_im/i18n.cljs
+++ src/status_im/i18n.cljs
@@ -1,5 +1,6 @@
 (ns status-im.i18n
   (:require
+    [cljs.spec.alpha :as s]
     [status-im.react-native.js-dependencies :as rn-dependencies]
     [status-im.translations.af :as af]
     [status-im.translations.ar :as ar]
@@ -52,80 +53,166 @@
     [status-im.translations.zh-yue :as zh-yue]
     [status-im.utils.js-resources :refer [default-contacts]]
     [taoensso.timbre :as log]
-    [clojure.string :as str]))
+    [clojure.string :as str]
+    [clojure.set :as set]))
 
 (set! (.-fallbacks rn-dependencies/i18n) true)
 (set! (.-defaultSeparator rn-dependencies/i18n) "/")
 
+;; translations
+
+(def translations-by-locale {:af          af/translations
+                             :ar          ar/translations
+                             :be          be/translations
+                             :cs          cs/translations
+                             :da          da/translations
+                             :de          de/translations
+                             :de-ch       de-ch/translations
+                             :en          en/translations
+                             :es          es/translations
+                             :es-ar       es-ar/translations
+                             :es-mx       es-mx/translations
+                             :fi          fi/translations
+                             :fr          fr/translations
+                             :fr-ch       fr-ch/translations
+                             :fy          fy/translations
+                             :he          he/translations
+                             :hi          hi/translations
+                             :hu          hu/translations
+                             :id          id/translations
+                             :it          it/translations
+                             :it-ch       it-ch/translations
+                             :ja          ja/translations
+                             :ko          ko/translations
+                             :la          la/translations
+                             :lt          lt/translations
+                             :lv          lv/translations
+                             :ms          ms/translations
+                             :nb          nb/translations
+                             :ne          ne/translations
+                             :nl          nl/translations
+                             :pl          pl/translations
+                             :pt-br       pt-br/translations
+                             :pt-pt       pt-pt/translations
+                             :ro          ro/translations
+                             :ru          ru/translations
+                             :sl          sl/translations
+                             :sr          sr-rs-cyrl/translations
+                             :sr-RS_#Latn sr-rs-latn/translations
+                             :sr-RS_#Cyrl sr-rs-cyrl/translations
+                             :sv          sv/translations
+                             :sw          sw/translations
+                             :th          th/translations
+                             :tr          tr/translations
+                             :uk          uk/translations
+                             :ur          ur/translations
+                             :vi          vi/translations
+                             :zh          zh-hans/translations
+                             :zh-hans     zh-hans/translations
+                             :zh-hans-tw  zh-hans/translations
+                             :zh-hans-sg  zh-hans/translations
+                             :zh-hans-hk  zh-hans/translations
+                             :zh-hans-cn  zh-hans/translations
+                             :zh-hans-mo  zh-hans/translations
+                             :zh-hant     zh-hant/translations
+                             :zh-hant-tw  zh-hant/translations
+                             :zh-hant-sg  zh-hant/translations
+                             :zh-hant-hk  zh-hant/translations
+                             :zh-hant-cn  zh-hant/translations
+                             :zh-hant-mo  zh-hant/translations
+                             :zh-wuu      zh-wuu/translations
+                             :zh-yue      zh-yue/translations})
+
+; english as source of truth
+(def trans-ids (set (keys en/translations)))
+
+(s/def ::trans-id trans-ids)
+(s/def ::trans-ids (s/coll-of ::trans-id :kind set? :into #{}))
+
+(defn trans-ids-for-all-locales []
+  (->> translations-by-locale
+       (mapcat #(-> % val keys))
+       set))
+
+;; checkpoints
+
+(s/def ::checkpoint.id keyword?)
+(s/def ::checkpoint-defs (s/map-of ::checkpoint.id ::trans-ids))
+
+(def checkpoint-1-trans-ids trans-ids)
+; these could be status versions instead of incremental numbers
+(def checkpoints-def (s/assert ::checkpoint-defs
+                               {::checkpoint|1 checkpoint-1-trans-ids
+                                ::checkpoint|2 (into checkpoint-1-trans-ids #{})}))
+(def checkpoints (set (keys checkpoints-def)))
+
+(s/def ::checkpoint checkpoints)
+
+(def checkpoint-to-consider-locale-supported ::checkpoint|1)
+
+(defn checkpoint-val-to-compare [c]
+  (-> c name (str/replace #"^.*\|" "") int))
+
+(defn >checkpoints [& cs]
+  (apply > (map checkpoint-val-to-compare cs)))
+
+;; locales
+
+(def locales (set (keys translations-by-locale)))
+
+(s/def ::locale locales)
+(s/def ::locales (s/coll-of ::locale :kind set? :into #{}))
+
+(def supported-locales (s/assert ::locales #{:en :es}))
+
+(s/def ::supported-locale supported-locales)
+(s/def ::supported-locales (s/coll-of ::supported-locale :kind set? :into #{}))
+
+(defn locale->trans-ids [locale]
+  (-> translations-by-locale (get locale) keys set))
+
+(defn locale->checkpoint [locale]
+  (let [locale-trans-ids (locale->trans-ids locale)
+        checkpoint (->> checkpoints-def
+                        (filter (fn [[checkpoint checkpoint-trans-ids]]
+                                  (set/subset? locale-trans-ids checkpoint-trans-ids)))
+                        ffirst)]
+    checkpoint))
+
+(defn locales-with-checkpoint []
+  (->> locales
+       (map (fn [locale]
+              [locale (locale->checkpoint locale)]))
+       (into {})))
+
+(defn actual-supported-locales []
+  (->> (locales-with-checkpoint)
+       (filter val)
+       (filter (fn [[l c]]
+                 (or (= c checkpoint-to-consider-locale-supported)
+                     (>checkpoints checkpoint-to-consider-locale-supported c))))
+       keys
+       set))
+
+(defn locales-with-full-support []
+  (->> locales
+       (filter (fn [l]
+                 (set/subset? trans-ids (locale->trans-ids l))))
+       set))
+
+(defn supported-locales-that-are-not-considered-supported []
+  (set/difference (actual-supported-locales) supported-locales))
+
+
 (set! (.-translations rn-dependencies/i18n)
-      (clj->js {:af          af/translations
-                :ar          ar/translations
-                :be          be/translations
-                :cs          cs/translations
-                :da          da/translations
-                :de          de/translations
-                :de-ch       de-ch/translations
-                :en          en/translations
-                :es          es/translations
-                :es-ar       es-ar/translations
-                :es-mx       es-mx/translations
-                :fi          fi/translations
-                :fr          fr/translations
-                :fr-ch       fr-ch/translations
-                :fy          fy/translations
-                :he          he/translations
-                :hi          hi/translations
-                :hu          hu/translations
-                :id          id/translations
-                :it          it/translations
-                :it-ch       it-ch/translations
-                :ja          ja/translations
-                :ko          ko/translations
-                :la          la/translations
-                :lt          lt/translations
-                :lv          lv/translations
-                :ms          ms/translations
-                :nb          nb/translations
-                :ne          ne/translations
-                :nl          nl/translations
-                :pl          pl/translations
-                :pt-br       pt-br/translations
-                :pt-pt       pt-pt/translations
-                :ro          ro/translations
-                :ru          ru/translations
-                :sl          sl/translations
-                :sr          sr-rs-cyrl/translations
-                :sr-RS_#Latn sr-rs-latn/translations
-                :sr-RS_#Cyrl sr-rs-cyrl/translations
-                :sv          sv/translations
-                :sw          sw/translations
-                :th          th/translations
-                :tr          tr/translations
-                :uk          uk/translations
-                :ur          ur/translations
-                :vi          vi/translations
-                :zh          zh-hans/translations
-                :zh-hans     zh-hans/translations
-                :zh-hans-tw  zh-hans/translations
-                :zh-hans-sg  zh-hans/translations
-                :zh-hans-hk  zh-hans/translations
-                :zh-hans-cn  zh-hans/translations
-                :zh-hans-mo  zh-hans/translations
-                :zh-hant     zh-hant/translations
-                :zh-hant-tw  zh-hant/translations
-                :zh-hant-sg  zh-hant/translations
-                :zh-hant-hk  zh-hant/translations
-                :zh-hant-cn  zh-hant/translations
-                :zh-hant-mo  zh-hant/translations
-                :zh-wuu      zh-wuu/translations
-                :zh-yue      zh-yue/translations}))
+      (clj->js translations-by-locale))
 
 ;:zh, :zh-hans-xx, :zh-hant-xx have been added until this bug will be fixed https://github.com/fnando/i18n-js/issues/460
 
 (def delimeters
   "This function is a hack: mobile Safari doesn't support toLocaleString(), so we need to pass
   this map to WKWebView to make number formatting work."
-  (let [n          (.toLocaleString (js/Number 1000.1))
+  (let [n (.toLocaleString (js/Number 1000.1))
         delimiter? (= (count n) 7)]
     (if delimiter?
       {:delimiter (subs n 1 2)
--- test/cljs/status_im/test/i18n.cljs
+++ test/cljs/status_im/test/i18n.cljs
@@ -1,6 +1,20 @@
 (ns status-im.test.i18n
   (:require [cljs.test :refer-macros [deftest is]]
-            [status-im.i18n :as i18n]))
+            [status-im.i18n :as i18n]
+            [clojure.set :as set]
+            [cljs.spec.alpha :as s]))
 
 (deftest label-options
   (is (not (nil? (:key (i18n/label-options {:key nil}))))))
+
+(deftest english-has-all-translation-ids
+  (is (s/valid? ::i18n/trans-ids (i18n/trans-ids-for-all-locales))
+      (->> i18n/locales
+           (remove #(s/valid? ::i18n/trans-ids (i18n/locale->trans-ids %)))
+           (map (fn [l]
+                  (str "Extra translation ids in locale " l "\n" (set/difference (i18n/locale->trans-ids l) i18n/trans-ids) "\n\n")))
+           (apply str))))
+
+(deftest supported-locales-are-actually-supported
+  (is (s/valid? ::i18n/supported-locales (i18n/actual-supported-locales))
+      (s/explain-str ::i18n/supported-locales (i18n/actual-supported-locales))))
